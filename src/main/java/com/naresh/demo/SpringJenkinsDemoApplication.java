277db51e7a5792b0b48bab377ed1b8e0c51cb812
unknown <madaraveninaresh@gmail.com>
2022-02-23 14:31:07 +0530

    third commit

--- src/main/java/com/naresh/demo/SpringJenkinsDemoApplication.java
+++ src/main/java/com/naresh/demo/SpringJenkinsDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJenkinsDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("test s");
 		SpringApplication.run(SpringJenkinsDemoApplication.class, args);
 	}
 
